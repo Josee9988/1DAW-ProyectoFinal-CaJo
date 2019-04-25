1240a958bb8020d3d01da831b120e7d1b802b99c
Jose <jgracia9988@gmail.com>
2019-04-25 16:54:05 +0200

    fix

--- Proyecto1DAW/src/main.java
+++ Proyecto1DAW/src/main.java
@@ -1,9 +1,33 @@
+import java.io.IOException;
 
-public class main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("/view/ui2.fxml"));
+			primaryStage.setScene(new Scene(root));
+			Image icon = new Image(getClass().getResourceAsStream("/jc-favicon.png"));
+			primaryStage.getIcons().add(icon);
+			primaryStage.setTitle("Componentes");
+			primaryStage.show();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.toString());
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
 	}
 
 }
