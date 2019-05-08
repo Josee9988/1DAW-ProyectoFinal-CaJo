85635b73e577fc24bbc0e32c6a07bec28f0263ec
Jose <jgracia9988@gmail.com>
2019-05-08 08:39:23 +0200

    Title + favicon

--- Proyecto1DAW/src/Principal.java
+++ Proyecto1DAW/src/Principal.java
@@ -3,23 +3,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class Principal extends Application{
-	
+public class Principal extends Application {
+
 	@Override
-	public void start(Stage primaryStage){
+	public void start(Stage primaryStage) {
 		Parent root;
 		try {
-			root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
+			root = FXMLLoader.load(this.getClass().getResource("/view/login.fxml"));
 			primaryStage.setScene(new Scene(root));
+
+			Image icon = new Image(this.getClass().getResourceAsStream("/jc-favicon.png"));
+			primaryStage.getIcons().add(icon);
+			primaryStage.setTitle("Proyecto Jose Carlos");
 			primaryStage.show();
 		} catch (IOException e) {
 			System.out.println(e.toString());
 		}
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		launch(args);
 	}
 
