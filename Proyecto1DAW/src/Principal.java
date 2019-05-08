74fdf10c461e456d04542fe5aba8f7124debbb17
Jose Gracia <jgracia9988@gmail.com>
2019-05-08 12:20:20 +0200

    Fix

--- Proyecto1DAW/src/Principal.java
+++ Proyecto1DAW/src/Principal.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,10 +16,11 @@ public class Principal extends Application {
 			root = FXMLLoader.load(this.getClass().getResource("/view/login.fxml"));
 			primaryStage.setScene(new Scene(root));
 
-			Image icon = new Image(this.getClass().getResourceAsStream("/jc-favicon.png"));
+			Image icon = new Image(this.getClass().getResourceAsStream("/view/jc-favicon.png"));
 			primaryStage.getIcons().add(icon);
 			primaryStage.setTitle("Proyecto Jose Carlos");
 			primaryStage.show();
+
 		} catch (IOException e) {
 			System.out.println(e.toString());
 		}
